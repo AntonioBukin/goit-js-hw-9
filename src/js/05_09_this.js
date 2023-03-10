16515c8e04ea5e7435797465106bae9990174a49
AntonioBukin <bav1905@gmail.com>
2023-03-10 11:54:28 +0200

    upd_this

--- src/js/05_09_this.js
+++ src/js/05_09_this.js
@@ -1,3 +1,8 @@
+// ! 1. Контекст (this) у функції визначається на момент
+//виклику, а не на момент створення.
+// ! 2. Контекстом ф-ції буде об'єкт який її викликав
+/*---------------------------------------------------------------*/
+
 //!Функція це об'ект і вона передається по ссилке
 //Ф-ція це важкий тип, коли ви ф-цію кудись хочете передати, ви передаєте не копію а ссилку.
 
@@ -8,7 +13,20 @@
 // const fnB = fnA;
 // console.log('fnB === fnA', fnB === fnA); //true
 /*---------------------------------------------------------------*/
+// const bookShelf = {
+//   authors: ['Бернард Корнуэлл', 'Роберт Шекли'],
+//   getAuthors() {
+//     return this.authors;
+//   },
+//   addAuthor(authorName) {
+//     this.authors.push(authorName);
+//   },
+// };
 
+// console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли"]
+// bookShelf.addAuthor('Ли Танит');
+// console.log(bookShelf.getAuthors()); // ["Бернард Корнуэлл", "Роберт Шекли", "Ли Танит"]
+/*---------------------------------------------------------------*/
 //Контекст this
 //Де і як була оголошена ф-ція немає ніякого значення на контекст.
 //Контект можна виявити в момент виклику ф-ції, якщо він не прив'язан явно.
@@ -94,3 +112,29 @@
 //const swapColor = makeChangeColor();
 
 //swapColor('blue'); //Який this?? правило номер два makeChangeColor() виклик без контексту
+/*---------------------------------------------------------------*/
+
+//Example 6
+//Напишіть метод calcTotalPrice(stoneName), який приймає назву каменю
+//і розраховує та повертає загальну вартість каменів з таким ім'ям,
+//ціною та кількістю з властивості stones.
+
+// const chopShop = {
+//   stones: [
+//     { name: 'Emerald', price: 1300, quantity: 4 },
+//     { name: 'Diamond', price: 2700, quantity: 3 },
+//     { name: 'Sapphire', price: 1400, quantity: 7 },
+//     { name: 'Ruby', price: 800, quantity: 2 },
+//   ],
+//   calcTotalPrice(stoneName) {
+//     const currentStone = this.stones.find(stone => stoneName === stone.name);
+//     //console.log(currentStone);
+
+//     return currentStone.price * currentStone.quantity;
+//   },
+// };
+
+// console.log(chopShop.calcTotalPrice('Emerald')); // 5200
+// console.log(chopShop.calcTotalPrice('Diamond')); // 8100
+// console.log(chopShop.calcTotalPrice('Sapphire')); // 9800
+// console.log(chopShop.calcTotalPrice('Ruby')); // 1600
