6628e5ca28172f2bcf156354d702b17894581241
AntonioBukin <bav1905@gmail.com>
2023-03-11 11:51:51 +0200

    upd1

--- src/js/05_09_this.js
+++ src/js/05_09_this.js
@@ -52,6 +52,41 @@
 
 // foo();
 /*---------------------------------------------------------------*/
+//this в callback-функциях
+
+// const customer = {
+//   firstName: 'Jacob',
+//   lastName: 'Mercer',
+//   getFullName() {
+//     console.log(this);
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() это вызов метода getFullName без объекта
+//   console.log(`Обрабатываем заявку от ${callback()}.`);
+// }
+
+// makeMessage(customer.getFullName.bind(customer)); // Обрабатываем заявку от Jacob Mercer.
+
+// //Варіант 2
+// const user = {
+//   firstName: 'Oleg',
+//   lastName: 'Dond',
+// };
+// makeMessage(customer.getFullName.bind(user)); //Обрабатываем заявку от Oleg Dond.
+
+//методи call, apply - викликають ф-цію на місці
+//метод bind - робить копію ф-ції
+
+//! При передачі ф-ції в якості колбєку, її контекст не зберігається
+//! Для прив'язки контексту використовується методи call, apply та bind
+//! Методи call, apply - викликають ф-цію на місці і вказують їй контекст (1ий аргумент)
+//! Метод bind - зв'язує, робить копію ф-ції і вказує контекст
+//  (використовуйте при передечі ф-ції в якості колбєку)
+
+/*---------------------------------------------------------------*/
 
 //Example 1
 // const fn = function () {
@@ -208,3 +243,34 @@
 
 // console.log(calculator.add()); //15
 // console.log(calculator.mult()); //50
+/*---------------------------------------------------------------*/
+
+//Example 9
+//Выполни рефакторинг методов объекта pizzaPalace,
+//расставив отсутствующие this в местах обращения к свойствам и методам объекта.
+
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+//   // Change code below this line
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+//   // Change code above this line
+// };
+
+// console.log(pizzaPalace.order('Smoked'));
+// console.log(pizzaPalace.order('Four meats'));
+// console.log(pizzaPalace.order('Big Mike'));
+// console.log(pizzaPalace.order('Viennese'));
+/*---------------------------------------------------------------*/
+
+//Example 10
