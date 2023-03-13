53517fe8ed4777657a931f2313d6f3c3a8506b37
AntonioBukin <bav1905@gmail.com>
2023-03-13 12:42:06 +0200

    upd3

--- src/js/05_09_this.js
+++ src/js/05_09_this.js
@@ -418,3 +418,26 @@
 /*---------------------------------------------------------------*/
 
 //Example 15
+//Добавь классу Car метод constructor который принимает три параметра:
+// !brand - марка автомобиля.
+// !model - модель автомобиля.
+// !price - цена автомобиля.
+// !Класс Car должен создавать объект с одноимёнными свойствами brand, model и price,
+// значениями которых должны быть переданные аргументы во время её вызова с оператором new.
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// const auto = new Car('brand', 'model', 'price');
+
+// console.log(new Car('Audi', 'Q3', 36000)); //{ brand: "Audi", model: "Q3", price: 36000 }
+// console.log(new Car('BMW', 'X5', 58900)); //{ brand: "BMW", model: "X5", price: 58900 }
+// console.log(new Car('Nissan', 'Murano', 31700)); //{ brand: "Nissan", model: "Murano", price: 31700 }
+/*---------------------------------------------------------------*/
+
+//Example 16
