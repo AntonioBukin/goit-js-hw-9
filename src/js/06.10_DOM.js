39405530ef39329fc66a7666e847e87f53f529cd
AntonioBukin <bav1905@gmail.com>
2023-03-14 16:31:00 +0200

    add_DOM

---
+++ src/js/06.10_DOM.js
@@ -0,0 +1,10 @@
+//document.querySelector - для пошука єлементів
+
+// const navOneEl = document.querySelector('.menu');
+// console.log(navOneEl); //знайти один єлементи
+//Якщо нічого незнайдено, поверне null
+
+// const navEl = document.querySelectorAll('.menu-item');
+// console.log(navEl); //знайти всі єлементи
+//Якщо нічого незнайдено, поверне порожній массив
+/*---------------------------------------------------------------*/
