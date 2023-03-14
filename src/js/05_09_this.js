d5c96bd860d0d266e52815c55b0f27254914e537
AntonioBukin <bav1905@gmail.com>
2023-03-14 10:49:31 +0200

    upd

--- src/js/05_09_this.js
+++ src/js/05_09_this.js
@@ -731,3 +731,36 @@
 /*---------------------------------------------------------------*/
 
 //Example 23
+//Выполни рефакторинг класса Car. Добавь публичное статическое свойство MAX_PRICE со значением 50000
+//- максимально допустимая цена автомобиля.
+
+//Добавь сеттеру price проверку передаваемого значения параметра newPrice.
+//Если оно больше чем MAX_PRICE, сеттер ничего не делает,
+//а если меньше или равно, то перезаписывает цену автомобиля.
+
+// class Car {
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if (newPrice <= Car.MAX_PRICE) this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
+// const audi = new Car({ price: 35000 });
+// console.log(audi.price); // 35000
+
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+
+// audi.price = 51000;
+// console.log(audi.price); // 49000
