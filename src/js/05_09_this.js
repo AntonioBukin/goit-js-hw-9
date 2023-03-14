8c8e36485fe03ea50a837fd46f839755eb7abfdb
AntonioBukin <bav1905@gmail.com>
2023-03-14 12:19:34 +0200

    upd

--- src/js/05_09_this.js
+++ src/js/05_09_this.js
@@ -826,3 +826,49 @@
 /*---------------------------------------------------------------*/
 
 //Example 26
+//Добавь классу Admin метод constructor, который принимает один параметр -
+//объект настроек с двумя свойствами email и accessLevel.
+
+//Добавь классу Admin публичное свойство accessLevel,
+//значение которого будет передаваться при вызове конструктора.
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//   }
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser',
+//   };
+
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+/*---------------------------------------------------------------*/
+
+//Example 27
