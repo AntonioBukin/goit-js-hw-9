df176366634de4fcbf0ef108f017beb8d2095b22
AntonioBukin <bav1905@gmail.com>
2023-03-15 14:10:18 +0200

    dom

--- src/js/06.10_DOM.js
+++ src/js/06.10_DOM.js
@@ -8,3 +8,39 @@
 // console.log(navEl); //знайти всі єлементи
 //Якщо нічого незнайдено, поверне порожній массив
 /*---------------------------------------------------------------*/
+//Practice
+
+//Example 1
+//Створити картку продукта
+//В класі продукта може бути product--on-sale i product--not-available
+
+// const product = {
+//   name: 'oranges',
+//   description: 'Suppose I tell you I like apples better than oranges.',
+//   price: 2000,
+//   available: true,
+//   onSale: true,
+// };
+
+// const productRef = document.createElement('article'); //зробили createElement через 'article'
+// productRef.classList.add('product');
+
+// const titleRef = document.createElement('h2');
+// titleRef.textContent = product.name;
+// titleRef.classList.add('product__name');
+
+// const descryRef = document.createElement('p');
+// descryRef.textContent = product.description;
+// descryRef.classList.add('product__descry');
+
+// const priceRef = document.createElement('p');
+// priceRef.textContent = `Ціна: ${product.price} грн`;
+// priceRef.classList.add('product__price');
+
+// console.log(productRef);
+// console.log(titleRef);
+// console.log(descryRef);
+// console.log(priceRef);
+
+// productRef.append(titleRef, descryRef, priceRef); // все зібрали в одне ціле
+// console.log(productRef);
