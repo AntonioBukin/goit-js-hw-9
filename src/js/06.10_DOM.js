2fa3867db0f85f11a9a2d6828c32083a37b629de
AntonioBukin <bav1905@gmail.com>
2023-03-16 17:23:35 +0200

    dom

--- src/js/06.10_DOM.js
+++ src/js/06.10_DOM.js
@@ -56,65 +56,102 @@
 //<p product__price>Ціна: 2000 грн </p>
 //</article>
 
-const products = [
-  {
-    name: 'Манго',
-    price: 83,
-    online: false,
-    description: 'Suppose I tell you I like apples.',
-  },
-  {
-    name: 'Полі',
-    price: 59,
-    online: true,
-    description: 'Suppose I tell you I like apples.',
-  },
-  {
-    name: 'Аякс',
-    price: 37,
-    online: false,
-    description: 'Suppose I tell you I like apples.',
-  },
-  {
-    name: 'Ківі',
-    price: 94,
-    online: true,
-    description: 'Suppose I tell you I like apples.',
-  },
-];
+// const products = [
+//   {
+//     name: 'Манго',
+//     price: 83,
+//     online: false,
+//     description: 'Suppose I tell you I like apples.',
+//   },
+//   {
+//     name: 'Полі',
+//     price: 59,
+//     online: true,
+//     description: 'Suppose I tell you I like apples.',
+//   },
+//   {
+//     name: 'Аякс',
+//     price: 37,
+//     online: false,
+//     description: 'Suppose I tell you I like apples.',
+//   },
+//   {
+//     name: 'Ківі',
+//     price: 94,
+//     online: true,
+//     description: 'Suppose I tell you I like apples.',
+//   },
+// ];
 
 //1) Зробили карточку товара
 
-const makeProductCard = ({ name, description, price }) => {
-  //деструктурізація
-  const productRef = document.createElement('article');
-  productRef.classList.add('product');
+// const makeProductCard = ({ name, description, price }) => {
+//   //деструктурізація
+//   const productRef = document.createElement('article');
+//   productRef.classList.add('product');
 
-  const titleRef = document.createElement('h2');
-  titleRef.textContent = name;
-  titleRef.classList.add('product__name');
+//   const titleRef = document.createElement('h2');
+//   titleRef.textContent = name;
+//   titleRef.classList.add('product__name');
 
-  const descryRef = document.createElement('p');
-  descryRef.textContent = description;
-  descryRef.classList.add('product__descry');
+//   const descryRef = document.createElement('p');
+//   descryRef.textContent = description;
+//   descryRef.classList.add('product__descry');
 
-  const priceRef = document.createElement('p');
-  priceRef.textContent = `Ціна: ${price} грн`;
-  priceRef.classList.add('product__price');
+//   const priceRef = document.createElement('p');
+//   priceRef.textContent = `Ціна: ${price} грн`;
+//   priceRef.classList.add('product__price');
 
-  productRef.append(titleRef, descryRef, priceRef);
+//   productRef.append(titleRef, descryRef, priceRef);
 
-  return productRef;
-};
+//   return productRef;
+// };
 
-console.log(makeProductCard(products[0]));
+// console.log(makeProductCard(products[0]));
 
 //2) Зробити багато карток товарів
 
-const elements = products.map(makeProductCard);
+// const elements = products.map(makeProductCard);
 
-console.log(elements);
+// console.log(elements);
 
-const productsContainerEl = document.querySelector('.js-products');
+// const productsContainerEl = document.querySelector('.js-products');
 
-productsContainerEl.append(...elements);
+// productsContainerEl.append(...elements);
+/*---------------------------------------------------------------*/
+
+//Example 3
+//Стоворити список технологій що вивчаються на курсі Fullstack за допомогою js
+//Зробити через метод **map** та **reduce**
+
+//Шаблон як повинно виглядати
+/*
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JS</li>
+  <li>React</li>
+  <li>Node</li>
+</ul>;
+*/
+
+const technologies = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+
+const listRef = document.createElement('ul'); //створюємо вузел ul
+
+for (const technology of technologies) {
+  //console.log('technology:', technology);
+  const liRef = document.createElement('li'); //створюємо li
+  //Варіант 1
+  //liRef.textContent = technology; // створили текстовый контент в середині элемента
+  //Варіант 2
+  const text = document.createTextNode(technology); //створюємо текстовий вузол
+  liRef.appendChild(text); //Додаємо текстовий вузов в DOM
+
+  listRef.appendChild(liRef); //створений текстовий контент додаємо до **listRef** (ul)
+}
+
+//тепер нам потрібно контейнер наш ul з li додати на сторінку
+document.body.appendChild(listRef);
+
+console.dir(listRef);
