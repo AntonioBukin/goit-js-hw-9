a91de416ae3e723baee8a7012e157153a8dc1005
AntonioBukin <bav1905@gmail.com>
2023-03-17 15:56:19 +0200

    dom

--- src/js/06.10_DOM.js
+++ src/js/06.10_DOM.js
@@ -246,3 +246,34 @@
 //   console.log(el.firstElementChild.textContent); //отримали доступ до всіх h2 + вивели заголовки
 //   console.log(el.lastElementChild.children.length); //вивели кількість категорій
 // });
+/*---------------------------------------------------------------*/
+
+//Variant - 2
+// const elItem = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${elItem.length}`);
+
+// elItem.forEach(el => {
+//   console.log(`Category: ${el.firstElementChild.textContent}`);
+//   console.log(`Elements: ${el.lastElementChild.children.length}`);
+// });
+/*---------------------------------------------------------------*/
+
+//Example 6
+//В HTML есть пустой список ul#ingredients.
+//<ul id="ingredients"></ul>
+//В JavaScript есть массив строк.
+
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
