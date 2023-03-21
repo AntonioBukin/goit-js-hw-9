4f147ce96fe45258cc9a68fbb3db9192e2d7cda4
AntonioBukin <bav1905@gmail.com>
2023-03-21 14:21:05 +0200

    add_evt

--- src/js/06_11_events.js
+++ src/js/06_11_events.js
@@ -1,32 +1,76 @@
 /*
  * - Створення і видаленння слухачів
  */
-const targetBtn = document.querySelector('.js-target-btn');
-const addListenerBtn = document.querySelector('.js-add-listener');
-const removeListenerBtn = document.querySelector('.js-remove-listener');
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
 
-addListenerBtn.addEventListener('click', () => {
-  console.log('Чіпляю слухача події на цільову кнопку');
+// addListenerBtn.addEventListener('click', () => {
+//   console.log('Чіпляю слухача події на цільову кнопку');
 
-  targetBtn.addEventListener('click', onTargetBtnClick);
-});
+//   targetBtn.addEventListener('click', onTargetBtnClick);
+// });
 
 //операція по видаленню слухача
 //якщо ви знаете, що будете відписуватись потрібно передати і на
 // додавання і виделення слухача одну і теж саму ф-цію.
 
-removeListenerBtn.addEventListener('click', () => {
-  console.log('Знімаю слухача події з цільової кнопки');
+// removeListenerBtn.addEventListener('click', () => {
+//   console.log('Знімаю слухача події з цільової кнопки');
 
-  targetBtn.removeEventListener('click', onTargetBtnClick);
-});
+//   targetBtn.removeEventListener('click', onTargetBtnClick);
+// });
 
-function onTargetBtnClick() {
-  console.log('клік по цільовий конопці');
-}
+// function onTargetBtnClick() {
+//   console.log('клік по цільовий конопці');
+// }
 
-function logMessage() {
-  console.log('Клік по цільовий кнопці');
-}
+// function logMessage() {
+//   console.log('Клік по цільовий кнопці');
+// }
 /*---------------------------------------------------------------*/
-//Variant - 2
+// Модальне вікно
+// 1. Открити і закрити по кнопці: onModalOpen та onModalClose
+// 2. Закрити по кліку в бекдроп on BackDropClick
+// 3. Закрити по ESC: onEscapeKeypress
+// 4. В СSS є клас show-modal, який необхідно додати на body при відкритті модалки.
+
+// const refs = {
+//   openModalBtn: document.querySelector('[data-action="open-modal"]'),
+//   closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+//   backdrop: document.querySelector('.js-backdrop'),
+// };
+
+// // 1. Открити і закрити по кнопці: onModalOpen та onModalClose
+
+// refs.openModalBtn.addEventListener('click', onOpenModal);
+// refs.closeModalBtn.addEventListener('click', onCloseModal);
+// refs.backdrop.addEventListener('click', onBackdropClick);
+
+// function onOpenModal() {
+//   window.addEventListener('keydown', onEscKeyPress);
+//   document.body.classList.add('show-modal');
+// }
+
+// function onCloseModal() {
+//   window.removeEventListener('keydown', onEscKeyPress);
+//   document.body.classList.remove('show-modal');
+// }
+
+// // 2. Закрити по кліку в бекдроп on BackDropClick
+
+// function onBackdropClick(event) {
+//   if (event.currentTarget === event.target) {
+//     console.log('клік в бекдроп!!!');
+//     onCloseModal();
+//   }
+// }
+
+// // 3. Закрити по ESC: onEscapeKeypress
+// function onEscKeyPress(event) {
+//   const ESC_KEY_CODE = 'Escape';
+
+//   if (event.code === ESC_KEY_CODE) {
+//     onCloseModal();
+//   }
+// }
