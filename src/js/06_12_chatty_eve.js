3ca952961bf1523fa342ad6db414c9653ac3a55c
AntonioBukin <bav1905@gmail.com>
2023-03-24 13:15:34 +0200

    chattyEve

--- src/js/06_12_chatty_eve.js
+++ src/js/06_12_chatty_eve.js
@@ -1,6 +1,34 @@
-//Mousemove та throttle
+//Mousemove та throttle - уповільнення
 
-const coordsOutputRef = document.querySelector('.js-coords');
-let mouseMoveCbInvocationCounter = 0;
+// const coordsOutputRef = document.querySelector('.js-coords');
+// let mouseMoveCbInvocationCounter = 0;
 
-function onMouseMove(event) {}
+// const throttledOnMouseMove = _.throttle(onMouseMove, 250);
+
+// window.addEventListener('mousemove', throttledOnMouseMove);
+
+// function onMouseMove(event) {
+//   mouseMoveCbInvocationCounter += 1;
+
+//   coordsOutputRef.textContent = `Кількість викликів onMouseMove: ${mouseMoveCbInvocationCounter},
+//         X: ${event.clientX},
+//         Y: ${event.clientY},
+//     `;
+// }
+/*-------------------------------------------------------------*/
+
+//Input та debounce - відкладений пошук
+
+// const inputRef = document.querySelector('.js-input');
+// const outputRef = document.querySelector('.js-output');
+// let inputCbInvocationCounter = 0;
+
+// inputRef.addEventListener('input', _.debounce(onInputChange, 2000));
+
+// function onInputChange(event) {
+//   inputCbInvocationCounter += 1;
+
+//   outputRef.textContent = `Кількість викликів onInputChange : ${inputCbInvocationCounter},
+//     Значення: ${event.target.value}`;
+// }
+/*-------------------------------------------------------------*/
