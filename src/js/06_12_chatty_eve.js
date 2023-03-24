be21dda236935b82aa6e60630d0be99aebd13e69
AntonioBukin <bav1905@gmail.com>
2023-03-24 15:31:11 +0200

    ch_eve

--- src/js/06_12_chatty_eve.js
+++ src/js/06_12_chatty_eve.js
@@ -32,3 +32,50 @@
 //     Значення: ${event.target.value}`;
 // }
 /*-------------------------------------------------------------*/
+//Example 1 (Фільтр з пошуком)
+/* покрокові дії:
+1) Зарендеріть розмітку елементів списку;
+2) Слухаємо зміни фільтру;
+3) Фільтруємо данні і рендерім нові елементи
+*/
+
+const tech = [
+  { label: 'HTML' },
+  { label: 'CSS' },
+  { label: 'JS' },
+  { label: 'React' },
+  { label: 'Node.JS' },
+  { label: 'Vue' },
+  { label: 'Redux' },
+];
+
+const refs = {
+  list: document.querySelector('.js-list'),
+  input: document.querySelector('#filter'),
+};
+
+refs.input.addEventListener('input', _.debounce(onFilterChange, 2000));
+
+const listItemsMarkup = createListItemsMarkup(tech);
+
+refs.list.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+  return items.map(item => `<li>${item.label}</li>`).join('');
+}
+
+function onFilterChange(evt) {
+  //console.log(evt.target.value);
+  const filter = evt.target.value.toLowerCase();
+
+  const filteredItems = tech.filter(t =>
+    t.label.toLowerCase().includes(filter)
+  );
+
+  //console.log(filteredItems);
+
+  const listItemsMarkup = createListItemsMarkup(filteredItems);
+  //console.log(listItemsMarkup);
+
+  refs.list.innerHTML = listItemsMarkup;
+}
