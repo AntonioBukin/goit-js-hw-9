c82228e018fc718cf47c0d264cca8e779fa2b235
AntonioBukin <bav1905@gmail.com>
2023-03-25 16:55:43 +0200

    ch_eve

--- src/js/06_12_chatty_eve.js
+++ src/js/06_12_chatty_eve.js
@@ -39,43 +39,79 @@
 3) Фільтруємо данні і рендерім нові елементи
 */
 
-const tech = [
-  { label: 'HTML' },
-  { label: 'CSS' },
-  { label: 'JS' },
-  { label: 'React' },
-  { label: 'Node.JS' },
-  { label: 'Vue' },
-  { label: 'Redux' },
-];
+// const tech = [
+//   { label: 'HTML' },
+//   { label: 'CSS' },
+//   { label: 'JS' },
+//   { label: 'React' },
+//   { label: 'Node.JS' },
+//   { label: 'Vue' },
+//   { label: 'Redux' },
+// ];
 
-const refs = {
-  list: document.querySelector('.js-list'),
-  input: document.querySelector('#filter'),
-};
+// const refs = {
+//   list: document.querySelector('.js-list'),
+//   input: document.querySelector('#filter'),
+// };
 
-refs.input.addEventListener('input', _.debounce(onFilterChange, 2000));
+// refs.input.addEventListener('input', _.debounce(onFilterChange, 2000));
 
-const listItemsMarkup = createListItemsMarkup(tech);
+// const listItemsMarkup = createListItemsMarkup(tech);
 
-refs.list.innerHTML = listItemsMarkup;
+// refs.list.innerHTML = listItemsMarkup;
 
-function createListItemsMarkup(items) {
-  return items.map(item => `<li>${item.label}</li>`).join('');
-}
+// function createListItemsMarkup(items) {
+//   return items.map(item => `<li>${item.label}</li>`).join('');
+// }
 
-function onFilterChange(evt) {
-  //console.log(evt.target.value);
-  const filter = evt.target.value.toLowerCase();
+// function onFilterChange(evt) {
+//   //console.log(evt.target.value);
+//   const filter = evt.target.value.toLowerCase();
 
-  const filteredItems = tech.filter(t =>
-    t.label.toLowerCase().includes(filter)
-  );
+//   const filteredItems = tech.filter(t =>
+//     t.label.toLowerCase().includes(filter)
+//   );
 
-  //console.log(filteredItems);
+//   //console.log(filteredItems);
 
-  const listItemsMarkup = createListItemsMarkup(filteredItems);
-  //console.log(listItemsMarkup);
+//   const listItemsMarkup = createListItemsMarkup(filteredItems);
+//   //console.log(listItemsMarkup);
 
-  refs.list.innerHTML = listItemsMarkup;
-}
+//   refs.list.innerHTML = listItemsMarkup;
+// }
+/*-------------------------------------------------------------*/
+//Example 2 (леніве завантаження)
+
+// if ('loading' in HTMLImageElement.prototype) {
+//   console.log('браузер підтримує lazyLoad');
+
+//   const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+//   lazyImages.forEach(img => {
+//     img.src = img.dataset.src;
+//   });
+// } else {
+//   console.log('браузер Не підтримує lazyLoad');
+
+//   const script = document.createElement('script');
+//   script.src =
+//     'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+//   script.integrity =
+//     'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+//   script.crossOrigin = 'anonymous';
+//   script.referrerPolicy = 'no-referrer';
+
+//   document.body.appendChild(script);
+// }
+
+// const lazyImages = document.querySelectorAll('img[data-src]');
+// //console.log(lazyImages);
+
+// lazyImages.forEach(Image => {
+//   Image.addEventListener('load', onImageLoaded, { once: true });
+// });
+
+// function onImageLoaded(evt) {
+//   console.log('Зображення завантажилось');
+//   evt.target.classList.add('appear');
+// }
