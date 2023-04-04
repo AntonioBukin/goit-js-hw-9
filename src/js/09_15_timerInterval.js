eedfdf8a5160081e6b5b5aa5a03842f430e77153
AntonioBukin <bav1905@gmail.com>
2023-04-04 18:35:07 +0300

    add_new info

--- src/js/09_15_timerInterval.js
+++ src/js/09_15_timerInterval.js
@@ -1,31 +1,68 @@
 import '../css/style.css';
+import BSN from 'bootstrap.native';
 
 //Example 1 (сповіщення)
-const NOTIFICATION_DELAY = 3000;
-let timeoutId = null;
+// const NOTIFICATION_DELAY = 3000;
+// let timeoutId = null;
+
+// const refs = {
+//   notification: document.querySelector('.js-alert'),
+// };
+
+// refs.notification.addEventListener('click', onNotificationClick);
+
+// showNotification();
+
+// function onNotificationClick() {
+//   hideNotification();
+//   clearTimeout(timeoutId);
+// }
+
+// function showNotification() {
+//   refs.notification.classList.add('is-visible');
+
+//   timeoutId = setTimeout(() => {
+//     console.log('закриваємо алерт автоматично, щоб не вісел');
+//     hideNotification();
+//   }, NOTIFICATION_DELAY);
+// }
+
+// function hideNotification() {
+//   refs.notification.classList.remove('is-visible');
+// }
+/*---------------------------------------------------------------*/
+
+//Example 2 (підпишись на розсилку)
 
 const refs = {
-  notification: document.querySelector('.js-alert'),
+  modal: document.querySelector('#subscription-modal'),
+  subscribeBtn: document.querySelector('button[data-subscribe]'),
 };
+const PROMPT_DELAY = 3000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let promptCounter = 0;
+let hasSubscribed = false;
+const modal = new BSN.Modal('#subscription-modal');
 
-refs.notification.addEventListener('click', onNotificationClick);
+openModal();
 
-showNotification();
+refs.modal.addEventListener('hide.bs.modal', openModal);
+refs.subscribeBtn.addEventListener('click', onSubscribeBtnClick);
 
-function onNotificationClick() {
-  hideNotification();
-  clearTimeout(timeoutId);
+function openModal() {
+  if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubscribed) {
+    console.log('Максимальное кол-во надоеданий или подписался');
+    return;
+  }
+
+  setTimeout(() => {
+    console.log('Открываем надоедалку');
+    modal.show();
+    promptCounter += 1;
+  }, PROMPT_DELAY);
 }
 
-function showNotification() {
-  refs.notification.classList.add('is-visible');
-
-  timeoutId = setTimeout(() => {
-    console.log('закриваємо алерт автоматично, щоб не вісел');
-    hideNotification();
-  }, NOTIFICATION_DELAY);
-}
-
-function hideNotification() {
-  refs.notification.classList.remove('is-visible');
+function onSubscribeBtnClick() {
+  hasSubscribed = true;
+  modal.hide();
 }
